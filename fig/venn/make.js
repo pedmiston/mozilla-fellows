d8338f589815da1a4d65df5e332b81b2f1bf2bca
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-07-11 22:35:38 -0500

    Make three panel venn diagram

--- fig/venn/make.js
+++ fig/venn/make.js
@@ -1,5 +1,10 @@
 var page = require('webpage').create();
 
+page.viewportSize = {
+  width: 900,
+  height: 300
+};
+
 page.open('http://localhost:8000/index.html', function() {
   page.render('../venn.png');
   phantom.exit();
